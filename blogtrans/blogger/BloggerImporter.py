9e43d15c5568d199308cbe77fe13434838d54506
gaagaaga <yzugsr@gmail.com>
2009-05-23 05:00:37 +0800

    blogger importer..

--- blogtrans/blogger/BloggerImporter.py
+++ blogtrans/blogger/BloggerImporter.py
@@ -16,26 +16,29 @@ class BloggerImporter :
         xml_data = f.read()
         f.close()
         
-        tree = ET.fromstring(xml_data)
-        for entry in tree.findall("entry") :
-            print entry
+        print(type(xml_data))
         
+        tree = ET.fromstring(xml_data)
+            
+        for entry in tree.findall("{http://www.w3.org/2005/Atom}entry") :
+            print entry.tag
+            
         """
-    self.author = ""
-    self.title = ""
-    self.date = datetime.today()
-    self.category = []
-    self.status = Article.PUBLISH
-    self.allow_comments = True
-    self.allow_pings = True
-    #self.convert_breaks = True
+        self.author = ""
+        self.title = ""
+        self.date = datetime.today()
+        self.category = []
+        self.status = Article.PUBLISH
+        self.allow_comments = True
+        self.allow_pings = True
+        #self.convert_breaks = True
 
-    self.body = ""
-    self.extended_body = ""
-    self.excerpt = ""
+        self.body = ""
+        self.extended_body = ""
+        self.excerpt = ""
 
-    self.comments = []
-    self.pings = []"""
+        self.comments = []
+        self.pings = []"""
         
         return blogdata
         
