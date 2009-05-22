c56c6496ebb5179b821c9eeb74905d4949835f08
gaagaaga <yzugsr@gmail.com>
2009-05-23 02:27:25 +0800

    testcase..

--- run_test.py
+++ run_test.py
@@ -41,23 +41,32 @@ class TestImporter(unittest.TestCase):
             blogdata = importer.parse()
     """
 
-def test_wretch_import(filename) :
-    #print "Parsing file %s" % filename
-    importer = WretchImporter(filename)
-    blogdata = importer.parse()
-
+def _make_import_testcase(importer_name, filename, path) :
+    importer_class = globals()["%sImporter" % importer_name]
+    def func() :
+        full_filename = path + filename
+        importer = importer_class(full_filename)
+        blogdata = importer.parse()
+    
+    return unittest.FunctionTestCase(func, description="Import %s : %s" % (importer_name, filename) )
+    
+def make_import_testcases(importer_name) :
+     
+    path = "TestData/%s/" % importer_name
+    cases = []
+    filenames = os.listdir(path)
+    for filename in filenames :
+        cases.append( _make_import_testcase(importer_name, filename, path) )
+    
+    return cases
+    
 def main() :
     
     suite = unittest.TestSuite()
-    
-    path = "TestData/Wretch/"
-    filenames = os.listdir(path)
-    
-    for filename in filenames :
-        filename = path + filename
-        func = lambda filename=filename: test_wretch_import(filename)
-        case = unittest.FunctionTestCase( func, description = filename)
-        suite.addTest(case)
+
+    suite.addTests( make_import_testcases("Wretch") )
+    suite.addTests( make_import_testcases("MT") )
+    suite.addTests( make_import_testcases("Blogger") )
     
     unittest.TextTestRunner(verbosity=2).run(suite)
 
