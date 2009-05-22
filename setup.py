89798805cf7f1d6b366b19760ac3afb53e0d0619
gaagaaga <yzugsr@gmail.com>
2009-05-23 03:42:30 +0800

    update readme and put into build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    data_files = ["LICENSE"],
+    data_files = ["README", "LICENSE"],
     console=["blogtrans.py"], 
     options = { "py2exe": {"includes" : ["_strptime"] } }
 )
